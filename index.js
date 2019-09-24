7c7a3282864e9dd9ac37ccb8a1dd7c06b7dc63f4
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-24 16:36:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,5 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation(input) {
-  let employee = document.querySelector(input);
-  
-  return employee;
+  return input.toString();
 }
